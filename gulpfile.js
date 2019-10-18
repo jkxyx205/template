10c56f9644c02365bc7f17d6c3061f15d63025a9
rick <1050216579@qq.com>
2019-10-18 18:35:33 +0800

    Init

--- gulpfile.js
+++ gulpfile.js
@@ -8,40 +8,15 @@ let cleanCSS = require('gulp-clean-css');
 
 var fs = require('fs');
 
-// function sync() {
-//    setTimeout(function() {
-//       fs.writeFile('db.js', 'var refresh =  ' + (++index), function(err){
-//           if(err) throw err;
-//       });
-//     }, 500)
-// }
-
-
-// var index = 0;
-gulp.task('sass-header', function() {
-     return gulp.src('header/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs 
+gulp.task('build', function() {
+     return gulp.src(
+     [
+     'core/**/*.scss',
+     '!core/headers/**'
+     ])
      .pipe(sass()) 
-     .pipe(gulp.dest('dist/tpl-css/header'))
-// sync()
-
-      
+     .pipe(gulp.dest('dist'))
 })
-
-gulp.task('sass-style', function() { 
-    return  gulp.src('style/**/sass/*.scss') // Gets all files ending with .scss in app/scss and children dirs 
-     .pipe(sass()) 
-     .pipe(gulp.dest('dist/tpl-css/style'))
-     // sync()
-})
-
-
-gulp.task('sass-footer', function() {
-    return gulp.src('footer/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs 
-     .pipe(sass()) 
-     .pipe(gulp.dest('dist/tpl-css/footer'))
-     // sync()
-})
-
 
 // 在命令行使用 gulp auto 启动此任务
 // gulp.task('auto', function () {
@@ -61,7 +36,7 @@ gulp.task('minify-css', () => {
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['sass-header'], function() {
+gulp.task('default', ['build'], function() {
     // gulp.task("minify-css")
     // console.log('red;')
 })
